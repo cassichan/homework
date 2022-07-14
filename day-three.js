5d60fa44463a4482c99ebebfb35f89d9f38a87a0
Cassandra Lee <cassichan@Cassandras-MacBook-Air.local>
2022-07-14 10:40:25 -0400

    created homework folder

---
+++ day-three.js
@@ -0,0 +1,32 @@
+
+// 1. Write a function that adds two numbers together.
+function sumNum (num1, num2) {
+    return num1 + num2;
+}
+console.log(sumNum(4, 5));
+
+
+// 2. Write a car object that has a modal and color.  Create a property called year then console.log all of the objects properties individually.
+
+let car = {
+    color: "White",
+    model: "Prius",
+}
+
+car.year = 2016;
+console.log(car.color);
+console.log(car.model);
+console.log(car.year);
+
+
+// 3. Write a function to get the first element in an array.
+
+let arrayOfFaveStuff = ['cats', 'coffee', 'Disney'];
+
+const getFirstElement = (arr) => {
+    return arr.shift();
+    // return arr[0]; -> also works
+};
+
+console.log(getFirstElement(arrayOfFaveStuff));
+
