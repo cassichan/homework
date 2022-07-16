21be36818f6e2b2a447c6d3912b244d61c4f4ef7
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-15 21:49:51 -0400

    adding homework day 5

---
+++ day-five.js
@@ -0,0 +1,27 @@
+// Write a function to convert temperatures from Fahrenheit to Celsius.
+
+let celsius;
+
+function convertCelsiusToFahrenheit(fahrenheit) {
+return celsius = 5/9 * (fahrenheit - 32);
+}
+
+convertCelsiusToFahrenheit(100)
+console.log(`It is ${celsius} degrees in Celsius.`)
+
+
+
+// Write a function that converts USD to any other currency of choice.
+
+
+
+// The FizzBuzz problem is a classic test given in coding interviews. The task is simple: Print integers 1 to N, but print "Fizz" if an integer is divisible by 3, "Buzz" if an integer is divisible by 5, and "FizzBuzz" if an integer is divisible by both 3 and 5.
+
+
+
+// You are given 2 arrays, one called shoppingList and one called cart.
+// Create a new version of shoppingList where the items in the cart have been removed.
+
+
+
+// given a sentence reverse order: "the sky is blue" -> "euld si yks eht"
