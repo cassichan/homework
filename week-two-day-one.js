3969d6d464908a6e95d88b61486083055aac0b3b
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-18 20:41:06 -0400

    adding homework for week-two-day-one

---
+++ week-two-day-one.js
@@ -0,0 +1,49 @@
+// What are the seven paradigms of the database?
+// 1. Key-value
+// 2. Wide-column
+// 3. Document
+// 4. Relational
+// 5. Graph
+// 6. Search
+// 7. Multi-model
+
+// Create a function that takes a base number and an exponent number and returns the calculation.
+
+function exponentiateNum(base, exponent) {
+    return base ** exponent;
+}
+
+console.log(exponentiateNum(4, 2))
+
+
+
+// Create a function that takes an array and a string as arguments and returns the index of the string.
+
+let testString = 'woof'
+let testArray = [30, false, 'woof', 10]
+
+function calculateIndexOfString(arr, str) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] === str) {
+            
+            return arr.indexOf(str)
+        }
+    }
+}
+
+console.log(calculateIndexOfString(testArray, testString))
+
+
+// Create a function that takes an array and returns the types of values (data types) in a new array.
+
+let myArr = [5, 'meow', 12, true]  //Test array
+
+function determineDataTypes (arr) {
+    let dataTypeArray = [];
+    for (i = 0; i < arr. length; i++) {
+        dataTypeArray.push(typeof arr[i]);
+    }
+    return dataTypeArray;
+}
+
+console.log(determineDataTypes(myArr))
